a94ad0a1914b5e683220ccab7225b369bee5e6dd
Andrei Shakarov <andrei.shakarov@gmail.com>
2019-03-24 00:09:14 +0300

    Initial commit

---
+++ src/main/java/com/bsuir/favref/FavrefBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir.favref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavrefBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavrefBackendApplication.class, args);
+    }
+
+}
